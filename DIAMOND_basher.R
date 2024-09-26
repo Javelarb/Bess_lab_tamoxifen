fbc7d4078b191c46b1fab7299c3bcd7691757b71
Julio Avelar-Barragan <julio.avelar-barragan@thermofisher.com>
2024-09-26 16:34:44 -0700

    Added README and revision updates

--- DIAMOND_basher.R
+++ DIAMOND_basher.R
@@ -1,11 +1,12 @@
 # Load necessary libraries
 library(tidyverse)
 library(readr)
+library(ggpubr)
 
 setwd("/Users/juli.avelar-barragan/Desktop/Project_bioinformatics/Bess_lab_tamoxifen/")
 
 # Get a list of all txt files in the directory
-file_list <- list.files(path = "HMGC279_diamond_out/", pattern = "\\.txt$", full.names = TRUE)
+file_list <- list.files(path = "HMGI3013_diamond_out/", pattern = "\\.txt$", full.names = TRUE)
 
 # Initialize an empty list to store data frames
 df_list <- list()
@@ -52,4 +53,75 @@ Gus_RC_wide = GUS_rc %>%
   mutate_all(~ coalesce(., 0)) %>%
   rownames_to_column(var = "SampleID")
 
-write.table(Gus_RC_wide, "HMGC279_RPM_counts.txt", quote = F, sep = "\t", row.names = F, col.names = T)
+#write.table(Gus_RC_wide, "HMGC279_RPM_counts.txt", quote = F, sep = "\t", row.names = F, col.names = T)
+
+#Supplemental coverage figure
+cov_df = combined_df %>%
+  filter(X2 == "SRS024549.1421-T1-C", 
+         !file_name %in% c("2126", "22b", "2621", "Stan"))
+
+gene_length = max(cov_df$X10)
+
+calculate_sample_coverage <- function(sample_df, gene_length) {
+  sample_df %>%
+    rowwise() %>%
+    do(data.frame(position = seq(.$X9, .$X10))) %>%
+    ungroup() %>%
+    count(position) %>%
+    complete(position = 1:gene_length, fill = list(n = 0)) %>%
+    select(position, coverage = n)
+}
+
+calculate_all_samples_coverage <- function(df) {
+  df %>%
+    group_by(file_name) %>%
+    group_modify(~ calculate_sample_coverage(.x, gene_length)) %>%
+    ungroup()
+}
+
+all_samples_coverage <- calculate_all_samples_coverage(cov_df)
+
+# Calculate average coverage across all samples
+average_coverage <- all_samples_coverage %>%
+  group_by(position) %>%
+  summarize(avg_coverage = mean(coverage))
+
+# Calculate overall average coverage
+overall_avg_coverage <- mean(average_coverage$avg_coverage)
+
+# Create the plot
+A = ggplot(average_coverage, aes(x = position, y = avg_coverage)) +
+  geom_line() +
+  annotate("text", x = Inf, y = Inf, 
+           label = sprintf("Overall Gene Coverage: %.2f", overall_avg_coverage),
+           hjust = 1, vjust = 1) +
+  labs(x = "Position in Gene", y = "Average Coverage", 
+       title = "B. fragilis", subtitle = "SRS024549.1421-T1-C") +
+  theme_bw()
+
+cov_df2 = combined_df %>%
+  filter(X2 == "SRS024388.120461-T1-C", 
+         !file_name %in% c("2126", "22b", "2621", "Stan"))
+
+all_samples_coverage2 <- calculate_all_samples_coverage(cov_df2)
+
+# Calculate average coverage across all samples
+average_coverage2 <- all_samples_coverage2 %>%
+  group_by(position) %>%
+  summarize(avg_coverage = mean(coverage))
+
+# Calculate overall average coverage
+overall_avg_coverage2 <- mean(average_coverage2$avg_coverage)
+
+# Create the plot
+B = ggplot(average_coverage2, aes(x = position, y = avg_coverage)) +
+  geom_line() +
+  annotate("text", x = Inf, y = Inf, 
+           label = sprintf("Overall Gene Coverage: %.2f", overall_avg_coverage2),
+           hjust = 1, vjust = 1) +
+  labs(x = "Position in Gene", y = "Average Coverage", 
+       title = "B. fragilis", subtitle = "SRS024388.120461-T1-C") +
+  theme_bw()
+
+ggsave(filename = "Supplemental_coverage_plot.png", plot = ggarrange(A, B, ncol = 1, labels = c("A", "B")),
+      dpi = 600, bg = "white", width = 6.54, height = 5)
---
+++ README.md
@@ -0,0 +1,14 @@
+# Read Me
+This repository contains the code and other files pertaining to the manuscript titled, "Variation in human gut microbiota impacts tamoxifen pharmacokinetics", by Alam et al.  
+
+A description of the files in the repository:  
+* Bess_Tamoxifen_analysis.Rmd contains the main R code used to perform statistical analysis and produce figures.  
+* DIAMOND_basher.R is an R script used to format the read count data from aligning sequences to the HMGC279 and HMGI3013 databases.  
+* Glucorunide_incubation_GUS.csv - Contains the total ion counts produced from donor fecal samples.  
+HMGxXXX_Characteristics.xlsx - Contains the corresponding databases's GUS gene characteristics.  
+* HMGxXXX_Sequences.xlsx - Contains the corresponding databases's amino acid sequences.  
+* HMGxXXX_RPM_counts.txt - Contains normalized read count data produced from aligning fecal donors to the corresponding database.  
+* Readinfo.csv - Contains information regarding the number of reads per sample.  
+* combined_bracken_XXX.txt - A table containing count information of each sample at various taxonomic levels. Produced by annotating reads using Kraken2 and Bracken.  
+
+Any questions should be directed to the corresponding author of the manuscript.  
